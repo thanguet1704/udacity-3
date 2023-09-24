35dacc2566867ef945205751cca4a88dcdaca762
trinhthang1704 <ngocthanhotboy@gmail.com>
2023-09-25 01:12:41 +0700

    upate part 2

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
-export URL=
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=thang12345
+export POSTGRES_HOST=database-2.cdyoqgcb8j3p.us-east-1.rds.amazonaws.com
+# export POSTGRES_HOST=localhost
+export POSTGRES_DB=udacity
+export AWS_BUCKET=arn:aws:s3:::app-1704-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
