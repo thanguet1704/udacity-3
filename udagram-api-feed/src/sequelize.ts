9138f98aa707d6d020147636ee4efe1bb10a38a6
trinhthang1704 <ngocthanhotboy@gmail.com>
2023-09-25 11:25:02 +0700

    update sequelize

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -2,10 +2,10 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
-  username: "postgres",
-  password: "thang12345",
-  database: "udacity",
-  host: "database-1.cdyoqgcb8j3p.us-east-1.rds.amazonaws.com",
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
 
   dialect: config.dialect,
   storage: ":memory:",
